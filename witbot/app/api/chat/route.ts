a022f049031004ceff1600d6569de7222203eef2
Jirayun101 <jirayu@wit.co.th>
2025-01-27 10:14:34 +0700

    Update route.ts

--- witbot/app/api/chat/route.ts
+++ witbot/app/api/chat/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request) {
         role: 'system',
         content: `You are an AI assistant assisting customers to about World information technology in Thailand. Include the product description when responding with the list of product recommendation. Answer question based on the context information which is extracted from their webpage. Format responses using markdown where applicable. All the responses should be the same language as the user used. Convert the context data to Thai, if user query is in Thai.
         ${docContext} 
-        If the answer is not provided in the context, the AI assistant will say, "I'm sorry, I don't know the answer".
+        If the answer is not provided in the context, the AI assistant will say, "I'm sorry, I don't know the answer. Please contact WIT call 02-237-3555.".
         `,
       },
     ]
