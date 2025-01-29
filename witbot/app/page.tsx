0fc56b50c7ab74fa96ae376f9a2a71f87aa7a4b4
Jirayun101 <jirayu@wit.co.th>
2025-01-29 15:06:18 +0700

    Revert "Test"
    This reverts commit a22c01d7e63df62e2541d81e89bd8289252c308e.

--- witbot/app/page.tsx
+++ witbot/app/page.tsx
@@ -51,7 +51,6 @@ export default function Home() {
         </div>
         <div className='flex-1 relative overflow-y-auto my-4 md:my-6'>
           <div className='absolute w-full overflow-x-hidden'>
-            Hello! I am WIT Assistance
             {messages.map((message, index) => <Bubble ref={messagesEndRef} key={`message-${index}`} content={message} />)}
           </div>
         </div>
