7b620ca689ad5da75d0bc8813c81a5e0b0fe7903
Jirayun101 <jirayu@wit.co.th>
2025-01-29 15:19:50 +0700

    Add Welcome message

--- witbot/app/page.tsx
+++ witbot/app/page.tsx
@@ -17,7 +17,6 @@ export default function Home() {
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
-
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
@@ -50,7 +49,7 @@ export default function Home() {
           <p className="chatbot-text-secondary-inverse text-sm md:text-base mt-2 md:mt-4">Chatting with the WIT AI Assistant is a breeze! Simply type your questions or requests in a clear and concise manner. Responses are driven by Retrival Augmented Generation (RAG) from relevant content stored in Astra vector database and LLM. Sometimes link may be provided for further reading is provided.</p>
         </div>
         <div className='flex-1 relative overflow-y-auto my-4 md:my-6'>
-          <div className='absolute w-full overflow-x-hidden'>
+          <div className='absolute w-full overflow-x-hidden'>Hello. I am WIT AI Assistant.I help you inquiry About WIT, All Product and Solution of WIT, Careers in WIT.
             {messages.map((message, index) => <Bubble ref={messagesEndRef} key={`message-${index}`} content={message} />)}
           </div>
         </div>
